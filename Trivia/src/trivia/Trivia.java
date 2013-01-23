9c51a8ff8f5b897dea4da835af9d50b2d53e2edd
dsmiller95 <dsmiller95@gmail.com>
2013-01-22 21:16:08 -0600

    Fixes small problems with clicking buttons

--- Trivia/src/trivia/Trivia.java
+++ Trivia/src/trivia/Trivia.java
@@ -130,48 +130,41 @@ public class Trivia extends Applet implements Runnable, MouseListener, KeyListen
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		engine.clickedAt(e.getX(), e.getY());
+		// TODO Auto-generated method stub
 	}
-
+	
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		engine.clickedAt(e.getX(), e.getY());
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	/**
